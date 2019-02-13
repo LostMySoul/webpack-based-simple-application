24bddd592c7f53ac4f6b17e9004ead1028e47115
Protsenko Danil <DeadBender27@gmail.com>
2019-02-13 11:13:11 +0600

    all 10 tasks done

--- src/operationExecutor.js
+++ src/operationExecutor.js
@@ -60,7 +60,7 @@ class OperationExecutor {
     thirdTaskExecute(arg) {
         let gendered = Object.assign({}, arg);
         gendered.obj1.relatives.forEach(relatives => {
-            relatives.gender = ''
+            relatives.gender = '';
         });
         return gendered;
     }
@@ -88,7 +88,7 @@ class OperationExecutor {
         let elemArr = [];
         for (let i = 0; i < elem.length; i++) {
             elem[i].style.backgroundColor = "red";
-            elemArr.push("elem class:" + elem[0].className.toString() + " && BG_color:" + elem[0].style.backgroundColor.toString())
+            elemArr.push("elem class:" + elem[i].className.toString() + " && BG_color:" + elem[i].style.backgroundColor.toString());
         }
         return elemArr.toString();
     }
@@ -100,10 +100,15 @@ class OperationExecutor {
      * @returns object that contains array of items that match the hostname on which the application is running
      */
     sixthTaskExecute(arg) {
-        /**
-         * Place your code here
-         */
-        return null;
+        let hosts = [];
+        for (let val of arg["hostNames"]) {
+            if (val.length >= 3) {
+                if (!(val.substring(val.length - 4) === ".com" || val.substring(val.length - 4) === ".org" || val.substring(val.length - 3) === ".ru")) {
+                    hosts.push(val);
+                }
+            }
+        }
+        return hosts;
     }
 
     /**
@@ -130,8 +135,16 @@ class OperationExecutor {
      */
     eighthTaskExecute(arg) {
         let merged = arg.arr1.concat(arg.arr2);
-        //todo: key-value array, if no value, set null
-        return null;
+        let keyValueArr = {};
+        for (let i = 0; i < merged.length; i += 2) {
+            if (merged[i + 1] !== undefined) {
+                keyValueArr[merged[i]] = merged[i];
+            } else {
+                keyValueArr[merged[i]] = null;
+            }
+        }
+
+        return keyValueArr;
     }
 
     /**
@@ -141,23 +154,33 @@ class OperationExecutor {
      * @returns obj that contains pairs id: obj with this id
      */
     ninthTaskExecute(arg) {
-        /**
-         * Place your code here
-         */
-        return null;
+        let arr = {};
+        for (let val of arg.users) {
+            let obj = {};
+            obj["firstName"] = val.firstName;
+            obj["lastName"] = val.lastName;
+            arr[val.id] = obj;
+        }
+        return arr;
     }
 
     /**
      * Tenth task of homework
      * @param arg – object which contains class of item and empty array
      * arg = { obj1: { ... } }
-     * @returns obj that contains the array with info about children of the node
+     * @returns Array that contains the array with info about children of the node
      */
     tenthTaskExecute(arg) {
-        /**
-         * Place your code here
-         */
-        return null;
+        let elem = document.getElementsByClassName(arg.className);
+        for (let i = 0; i < elem.length; i++) {
+            for (let j = 0; j < elem[i].children.length; j++) {
+                let obj = {};
+                obj["className"] = elem[i].children[j].className;
+                obj["tagName"] = elem[i].children[j].tagName;
+                arg["childrenInfo"].push(obj);
+            }
+        }
+        return arg;
     }
 }
 
